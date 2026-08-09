f156bc295acc0dd554073ee7cb7afe932f507f11
Ayush Singh <138690808+ayushsingh0702@users.noreply.github.com>
2026-08-09 15:56:43 +0530

    Add: remove-element - submission-0

---
+++ Data Structures & Algorithms/remove-element/submission-0.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int left = 0;
+        int res =0;
+        for(int i =0 ; i< nums.length;i++){
+            if(nums[i]!=val){
+                res++;
+                int temp = nums[i];
+                nums[i]= nums[left];
+                nums[left]= temp;
+                left++;
+            }
+        }
+        return res;
+    }
+}
